fc5cfc9d9cc4cefbaf030c741a0c5307f9d452b1
hy <dkgk2233@gmail.com>
2025-07-18 10:38:29 +0900

    .

--- vllm/model_executor/model_loader/custom_loader.py
+++ vllm/model_executor/model_loader/custom_loader.py
@@ -144,8 +144,8 @@ class CustomLoader(BaseModelLoader):
                 filepaths += s3_glob(path=local_model_path,
                                      allow_pattern=[file_pattern])
             else:
+                print(f"🔴[Rank {rank}] Tag {tag} filepaths: {glob.glob(pattern)}")
                 filepaths += glob.glob(pattern)
-                print(f"🔴[Rank {rank}] filepaths: {glob.glob(pattern)}")
                 print(f"🅰️[Rank {rank}] Tag {tag} found files: {filepaths}")
 
         # 로컬에 없는 shard(tag) → Ray로 다른 노드에서 가져오기
