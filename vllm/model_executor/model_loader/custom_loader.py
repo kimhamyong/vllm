7dc86abdf07a79e3adf0d8a22ffc425d7ac39697
hy <dkgk2233@gmail.com>
2025-07-21 09:25:35 +0900

    다시 true로

--- vllm/model_executor/model_loader/custom_loader.py
+++ vllm/model_executor/model_loader/custom_loader.py
@@ -182,7 +182,7 @@ class CustomLoader(BaseModelLoader):
                         num_cpus=0,
                         scheduling_strategy=NodeAffinitySchedulingStrategy(
                            node_id=n["NodeID"],
-                           soft=False,
+                           soft=True,
                         ),
                     ).remote(local_model_path, tag, self.pattern)
                     for n in ray.nodes()
