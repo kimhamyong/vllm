2d1983819a6e211a163b6bb49ecd230282112db8
hy <dkgk2233@gmail.com>
2025-07-21 10:12:03 +0900

    t

--- vllm/model_executor/model_loader/custom_loader.py
+++ vllm/model_executor/model_loader/custom_loader.py
@@ -128,7 +128,7 @@ class CustomLoader(BaseModelLoader):
             )
         else:                                             # rank ≥ half
             desired_tags = (
-                f"{rank}0", f"{rank}1",                               # 자기 1번만
+                f"{rank}0",                               # 자기 1번만
             )
 
         print(f"🅾️[Rank {rank}] Desired tags: {desired_tags}")
